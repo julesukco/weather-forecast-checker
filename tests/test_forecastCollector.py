f5d5f3abf9842d4aa813d17a00d02623bf39d2a0
Julian Bishop <jb@customsite.com>
2018-12-15 19:29:15 -0700

    tested row of data

--- tests/test_forecastCollector.py
+++ tests/test_forecastCollector.py
@@ -2,8 +2,8 @@ import unittest
 
 from forecastCollector import forecastCollector
 
-def test_format_data():
-    row = ('no data')
-    forecastCollector.getWeatherData()
-    assert True
+def test_rowOfData():
+    row = ("201812151700,20181216,1,56.0,31.1,0.25,0")
+    data = forecastCollector.getWeatherData()
+    assert row == data
 
