827698d900a27a7df9a5d92962010b01ed8dea3b
Julian Bishop <jb@customsite.com>
2018-12-15 19:37:36 -0700

    setup workspace

--- tests/test_forecastCollector.py
+++ tests/test_forecastCollector.py
@@ -4,6 +4,6 @@ from forecastCollector import forecastCollector
 
 def test_rowOfData():
     row = ("201812151700,20181216,1,56.0,31.1,0.25,0")
-    data = forecastCollector.getWeatherData()
+    data = forecastCollector.rowOfData()
     assert row == data
 
