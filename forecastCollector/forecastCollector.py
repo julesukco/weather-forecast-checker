3806826879f3ab3c0e5dc909e83ed69a50ff6698
Julian Bishop <jb@customsite.com>
2018-12-15 19:25:34 -0700

    added rowOfData()

--- forecastCollector/forecastCollector.py
+++ forecastCollector/forecastCollector.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 def getWeatherData():
     return 
 
+def rowOfData():
+    return("201812151700,20181216,1,56.0,31.1,0.25,0")
+
 if __name__ == "__main__":
     getWeatherData()
     print("Done")
